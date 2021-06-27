8946b6cbb9985d70ce7261016d757b435a2c67d6
AlexJustiDLC <alex_justiniano_delacruz_8@hotmail.com>
2021-06-26 20:24:46 -0500

    second commit

--- src/main/java/com/matricula/api/config/SwaggerInfo.java
+++ src/main/java/com/matricula/api/config/SwaggerInfo.java
@@ -3,6 +3,8 @@ package com.matricula.api.config;
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -41,4 +43,18 @@ public class SwaggerInfo {
                 Collections.emptyList()
         );
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT")
+                ;
+            }
+        };
+    }
+
 }
